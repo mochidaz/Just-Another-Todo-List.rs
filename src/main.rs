110c201280724bc83b9b23fec261659407618b5f
Rahman Hakim <rahmanhakim2435@gmail.com>
2022-03-19 09:35:26 +0700

    tui constructions

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use chrono::prelude::*;
-use clap::{App, Arg, ArgMatches};
 use std::time::SystemTime;
 use todo::{delete_todo, edit_todo};
 
@@ -12,166 +11,45 @@ mod parser;
 mod todo;
 mod utils;
 
-fn args() -> ArgMatches<'static> {
-    App::new("Todo")
-        .version("0.1.0")
-        .author("Rahman Hakim <rahmanhakim2435@protonmail.com")
-        .about("Todo List")
-        .arg(
-            Arg::with_name("title")
-                .short("t")
-                .long("title")
-                .takes_value(true)
-                .help("Title of the todo")
-                .required(false),
-        )
-        .arg(
-            Arg::with_name("content")
-                .short("c")
-                .long("content")
-                .takes_value(true)
-                .help("Content of the Todo")
-                .required(false),
-        )
-        .arg(
-            Arg::with_name("show")
-                .short("s")
-                .long("show")
-                .takes_value(false)
-                .help("Show todos")
-                .required(false),
-        )
-        .arg(
-            Arg::with_name("edit")
-                .short("e")
-                .long("edit")
-                .takes_value(true)
-                .help("Edit todo")
-                .required(false),
-        )
-        .arg(
-            Arg::with_name("delete")
-                .short("d")
-                .long("delete")
-                .takes_value(true)
-                .help("Delete a todo. todo -d <todo number>")
-                .required(false),
-        )
-        .get_matches()
+use crossterm::{
+    event::{self, DisableMouseCapture, EnableMouseCapture, Event, KeyCode},
+    execute,
+    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
+};
+use std::{error::Error, io};
+use tui::{
+    backend::{Backend, CrosstermBackend},
+    layout::{Constraint, Direction, Layout},
+    style::{Color, Modifier, Style},
+    text::{Span, Spans, Text},
+    widgets::{Block, Borders, List, ListItem, Paragraph},
+    Frame, Terminal,
+};
+use tui::layout::Alignment;
+use tui::widgets::BorderType;
+use unicode_width::UnicodeWidthStr;
+
+struct App {
+
 }
 
 fn main() {
-    let number = 1;
-    let filename = "todo.txt";
-    let system_time = SystemTime::now();
-    let time: DateTime<Local> = system_time.into();
-    let arguments = args();
-    let show_exists = arguments.is_present("show");
-    let content_exists = arguments.is_present("content");
-    let delete_exists = arguments.is_present("delete");
-    let title_exists = arguments.is_present("title");
-    let edit_exists = arguments.is_present("edit");
-    let edit = arguments.value_of("edit").unwrap_or("0").to_string();
-    let content = arguments.value_of("content").unwrap_or("New").to_string();
-    let delete = arguments.value_of("delete").unwrap_or("Default");
-    let title = arguments
-        .value_of("title")
-        .unwrap_or("New Project")
-        .to_string();
 
-    if !edit_exists {
-        match (title_exists, content_exists, delete_exists) {
-            (true, true, _) => {
-                let mut t = Todo {
-                    number,
-                    title: title.clone(),
-                    content: content.clone(),
-                    date: time.format("%d/%m/%Y %T").to_string(),
-                };
-                generate_number(&mut t, filename);
-                write_json(&mut t, filename);
-            }
-            (false, true, _) => {
-                let mut t = Todo {
-                    number,
-                    title: "Untitled Todo".to_string(),
-                    content: content.clone(),
-                    date: time.format("%d/%m/%Y %T").to_string(),
-                };
-                generate_number(&mut t, filename);
-                write_json(&mut t, filename);
-            }
-            (true, false, _) => {
-                let mut t = Todo {
-                    number,
-                    title: title.clone(),
-                    content: "...".to_string(),
-                    date: time.format("%d/%m/%Y %T").to_string(),
-                };
-                generate_number(&mut t, filename);
-                write_json(&mut t, filename);
-            }
-            (false, false, true) => {}
-            (false, false, false) => {
-                if !show_exists {
-                    println!("Why do you even use this, captain?")
-                }
-            }
-        }
-    } else {
-        match (title_exists, content_exists) {
-            (true, true) => {
-                match edit_todo(
-                    filename,
-                    edit.parse::<i32>().unwrap(),
-                    &Some(title.to_string()),
-                    &Some(content.to_string()),
-                ) {
-                    Status::Success => println!("Edit success"),
-                    Status::Failed => println!("Edit Failed")
-                }
-            }
-            (true, false) => {
-                match edit_todo(
-                    filename,
-                    edit.parse::<i32>().unwrap(),
-                    &Some(title.to_string()),
-                    &None,
-                ) {
-                    Status::Success => println!("Edit success"),
-                    Status::Failed => println!("Edit Failed")
-                }
-            }
-            (false, true) => {
-                match edit_todo(
-                    filename,
-                    edit.parse::<i32>().unwrap(),
-                    &None,
-                    &Some(content.to_string()),
-                ) {
-                    Status::Success => println!("Edit success"),
-                    Status::Failed => println!("Edit Failed")
-                }
-            }
-            (false, false) => {
-                println!("Whatchu wanna edit lol?")
-            }
-        }
-    }
+}
 
-    if show_exists {
-        show_todos(filename);
-    }
+fn ui<B: Backend>(f: &mut Frame<B>) {
+    let size = f.size();
 
-    if delete_exists {
-        let del = delete_todo(delete.parse::<usize>().unwrap(), filename);
-        match del {
-            Status::Success => {
-                println!("Successfully deleted");
-            }
-            Status::Failed => {
-                println!("The todo with following number does not exist!")
-            }
-        };
-    }
+    let block = Block::default()
+        .borders(Borders::BOTTOM)
+        .title("Test")
+        .title_alignment(Alignment::Left)
+        .border_type(BorderType::Plain);
+
+    f.render_widget(block, size);
+
+    let chunks = Layout::default()
+        .direction(Direction::Horizontal)
+        .margin(5)
+        .constraints(C)
 }
